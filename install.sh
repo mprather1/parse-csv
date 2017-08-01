7916b0ba35d9a1348ac29dcbf91b601ef7076773
Michael Prather <mprather1@gmail.com>
2017-08-01 19:18:55 +0000

    update install file

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+yarn install && \
 printf "#!/usr/bin/env bash\nHOME=\$HOME /usr/local/bin/node $(pwd)/index.js \$(pwd) \$1" > parse-csv && \
 chmod +x parse-csv && \
 sudo cp parse-csv /usr/bin && \
